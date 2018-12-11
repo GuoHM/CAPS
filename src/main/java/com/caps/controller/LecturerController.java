083bb3809b566616702c93ec4e05b50e12662224
Guo Haoming <fsguohaoming@hotmail.com>
2018-12-11 15:08:51 +0800

    add drop down on enrollment page

--- src/main/java/com/caps/controller/LecturerController.java
+++ src/main/java/com/caps/controller/LecturerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.caps.entity.Course;
 import com.caps.service.LecturerService;
@@ -17,19 +18,27 @@ import com.caps.service.LecturerService;
 public class LecturerController {
 	@Autowired
 	LecturerService lecturerservice;
+
 	@RequestMapping("/welcome")
 	public String login() {
 		return "/lecturer/welcome";
 	}
+
 	@RequestMapping("/lecturer-course")
-	public String listCourses() {	
+	public String listCourses() {
 		return "/lecturer/lecturer-course";
 	}
 	
+	@RequestMapping("/view-enrollment")
+	public ModelAndView viewEnrollment(HttpSession httpsession) {
+		ModelAndView mav = new ModelAndView("/lecturer/view-enrollment");
+		mav.addObject("courselist", lecturerservice.findCourseByLecturer(httpsession));
+		return mav;
+	}
+
 	@RequestMapping("/api/listcourse")
 	@ResponseBody
-	public List<Course> listCourses(HttpSession httpsession) {	
+	public List<Course> listCourses(HttpSession httpsession) {
 		return lecturerservice.findCourseByLecturer(httpsession);
 	}
 }
-

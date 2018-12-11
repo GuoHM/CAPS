45d9053b506d10f6e6ad210c1b34f902663aa25d
Guo Haoming <fsguohaoming@hotmail.com>
2018-12-11 12:05:57 +0800

    add a method in courseDao

--- src/main/java/com/caps/dao/CourseDao.java
+++ src/main/java/com/caps/dao/CourseDao.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.caps.entity.Account;
 import com.caps.entity.Course;
 
 public interface CourseDao extends JpaRepository<Course, Integer> {
 	List<Course> findByCourseid(int courseid);
+	
+	List<Course> findByAccount(Account account);
 }
--- src/main/java/com/caps/util/UserUtil.java
+++ src/main/java/com/caps/util/UserUtil.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserUtil {
-	//session 由controller 注入参数传入
 	public static String currentUser(HttpSession session) {
 		SecurityContextImpl securityContext = (SecurityContextImpl) session.getAttribute("SPRING_SECURITY_CONTEXT");
 		return ((UserDetails)securityContext.getAuthentication().getPrincipal()).getUsername();

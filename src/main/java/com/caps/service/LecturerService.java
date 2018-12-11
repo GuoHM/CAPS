b8e946ede23e5ae197894e79c7cdbe0486b4cbee
Guo Haoming <fsguohaoming@hotmail.com>
2018-12-11 16:11:39 +0800

    view enrollment

--- src/main/java/com/caps/service/LecturerService.java
+++ src/main/java/com/caps/service/LecturerService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import com.caps.entity.Account;
 import com.caps.entity.Course;
 
 public interface LecturerService {
 	List<Course> findCourseByLecturer(HttpSession httpsession);
+	
+	List<Account> findStudentsByCourseid(int courseid);
 }
--- src/main/java/com/caps/service/impl/LecturerServiceImpl.java
+++ src/main/java/com/caps/service/impl/LecturerServiceImpl.java
@@ -1,5 +1,8 @@
 package com.caps.service.impl;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -9,8 +12,10 @@ import org.springframework.stereotype.Service;
 
 import com.caps.dao.AccountDao;
 import com.caps.dao.CourseDao;
+import com.caps.dao.EnrollmentDao;
 import com.caps.entity.Account;
 import com.caps.entity.Course;
+import com.caps.entity.Enrollment;
 import com.caps.service.LecturerService;
 import com.caps.util.UserUtil;
 
@@ -18,16 +23,30 @@ import com.caps.util.UserUtil;
 public class LecturerServiceImpl implements LecturerService {
 	@Autowired
 	CourseDao courseDao;
-	
+
 	@Autowired
 	AccountDao userDao;
-	
+
+	@Autowired
+	EnrollmentDao enrollmentDao;
+
 	@Override
 	public List<Course> findCourseByLecturer(HttpSession httpsession) {
 		// TODO Auto-generated method stub
 		String userid = UserUtil.currentUser(httpsession);
 		Account account = userDao.findByUserid(Integer.parseInt(userid));
 		return courseDao.findByAccount(account);
+	}
+
+	@Override
+	public List<Account> findStudentsByCourseid(int courseid) {
+		// TODO Auto-generated method stub
+		List<Enrollment> enrollment = enrollmentDao.findByIdCourseid(courseid);
+		List<Account> result = new ArrayList<Account>();
+		for (Enrollment e : enrollment) {
+			result.add(userDao.findByUserid(e.getId().getUserid()));
+		}
+		return result;
 	}
 
 }

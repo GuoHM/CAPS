1256b50b506f787aebd4887122abbf36a885abd2
Guo Haoming <fsguohaoming@hotmail.com>
2018-12-18 15:52:05 +0800

    fix

--- src/main/java/com/caps/dao/AccountRoleDao.java
+++ src/main/java/com/caps/dao/AccountRoleDao.java
@@ -1,13 +1,9 @@
 package com.caps.dao;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 import com.caps.entity.Accountrole;
 
 public interface AccountRoleDao extends JpaRepository<Accountrole, Integer> {
-	
-	
 
 }
